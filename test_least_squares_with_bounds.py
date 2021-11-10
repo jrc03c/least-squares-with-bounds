43e82c04c1d1575bc381a10e150d9f17548812d1
jrc03c <jrc03c@pm.me>
2021-11-10 10:20:43 -0600

    ...

--- test_least_squares_with_bounds.py
+++ test_least_squares_with_bounds.py
@@ -1,13 +1,12 @@
 import unittest
 from least_squares_with_bounds import leastSquaresWithBounds
-from numpy import dot
+from numpy import dot, min, max
 from numpy.random import seed, normal
 from pyds import flatten, rScore
 
 
 class LeastSquaresWithBoundsTestCase(unittest.TestCase):
-    def test(self):
-        seed(12345)
+    def testWithoutBounds(self):
         a = normal(size=[100, 50])
         x = normal(size=[50, 25])
         b = dot(a, x)
@@ -15,6 +14,18 @@ class LeastSquaresWithBoundsTestCase(unittest.TestCase):
 
         xPred = leastSquaresWithBounds(a, b, bounds=None)
         self.assertGreater(rScore(x, xPred), 0.95)
+
+    def testWithBounds(self):
+        a = normal(size=[100, 50])
+        x = normal(size=[50, 25])
+        b = dot(a, x)
+        b += 0.01 * normal(size=b.shape)
+        bounds = [(0, 1) for value in flatten(x)]
+
+        xPred = leastSquaresWithBounds(a, b, bounds=bounds)
+        self.assertGreater(rScore(x, xPred), 0.5)
+        self.assertGreaterEqual(min(xPred), 0)
+        self.assertLessEqual(max(xPred), 1)
 
     def testErrors(self):
         pass
