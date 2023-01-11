15b0da09dd82b9939050eb69bfbb8825f427bba4
jrc03c <jrc03c@pm.me>
2023-01-11 09:25:08 -0600

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="least_squares_with_bounds",
-    version="0.0.3",
+    version="0.0.4",
     description="just a little wrapper around scipy.optimize.minimize",
     url="https://github.com/jrc03c/least-squares-with-bounds",
     author="jrc03c",
@@ -10,5 +10,7 @@ setup(
     license="none",
     packages=["least_squares_with_bounds"],
     install_requires=["numpy", "scipy", "pyds @ git+https://github.com/jrc03c/pyds"],
-    classifiers=["Topic :: Scientific/Engineering :: Mathematics",],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
 )
